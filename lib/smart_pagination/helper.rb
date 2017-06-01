1d1bb3e12bb3e20517210dfa1d3c64421d713aad
Jonian Guveli <jonian@hardpixel.eu>
2017-06-01 13:16:49 +0300

    add smart_pager_for helper

--- lib/smart_pagination/helper.rb
+++ lib/smart_pagination/helper.rb
@@ -7,5 +7,14 @@ module SmartPagination
 
     # Alias helper method
     alias :pagination_for :smart_pagination_for
+
+    # Render pager links
+    def smart_pager_for(collection, options={})
+      options = options.merge(pager_mode: true)
+      SmartPagination::Renderer.new(self, collection, options).render
+    end
+
+    # Alias helper method
+    alias :pager_for :smart_pager_for
   end
 end
