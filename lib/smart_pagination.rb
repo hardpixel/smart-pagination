afa48c657fca0dd2023074af3b890eb57ca6f2f0
Jonian Guveli <jonian@hardpixel.eu>
2017-11-14 20:15:54 +0200

    use active_support autoload

--- lib/smart_pagination.rb
+++ lib/smart_pagination.rb
@@ -1,16 +1,16 @@
 require 'smart_paginate'
+require 'active_support'
 require 'smart_pagination/version'
-require 'smart_pagination/helper'
-require 'smart_pagination/renderer'
 
 module SmartPagination
-  class << self
-    def included(model_class)
-      model_class.send :include, SmartPaginate
-    end
-  end
+  extend ActiveSupport::Autoload
+
+  # Autoload modules
+  autoload :Renderer
+  autoload :Helper
 end
 
+# Include action view helpers
 if defined? ActionView::Base
   ActionView::Base.send :include, SmartPagination::Helper
 end
