96c30f3c1fe9e0ff59cc987156c86e3f3f83e8c3
Jonian Guveli <jonian@hardpixel.eu>
2019-01-07 13:21:22 +0200

    remove useless comments

--- lib/smart_pagination/helper.rb
+++ lib/smart_pagination/helper.rb
@@ -1,29 +1,23 @@
 module SmartPagination
   module Helper
-    # Render pagination links
     def smart_pagination_for(collection, options = {})
       SmartPagination::Renderer.new(self, collection, options).render
     end
 
-    # Alias helper method
     alias :pagination_for :smart_pagination_for
 
-    # Render pager links
     def smart_pager_for(collection, options = {})
       options = options.merge(pager_mode: true)
       SmartPagination::Renderer.new(self, collection, options).render
     end
 
-    # Alias helper method
     alias :pager_for :smart_pager_for
 
-    # Render pagination info text
     def smart_pagination_info_for(collection, options = {})
       options = options.merge(info_mode: true)
       SmartPagination::Renderer.new(self, collection, options).render
     end
 
-    # Alias helper method
     alias :pagination_info_for :smart_pagination_info_for
   end
 end
--- lib/smart_pagination/renderer.rb
+++ lib/smart_pagination/renderer.rb
@@ -1,13 +1,11 @@
 module SmartPagination
   class Renderer
-    # Initialize pagination renderer
     def initialize(context, collection, options = {})
       @context    = context
       @collection = collection
       @options    = default_options.merge(options)
     end
 
-    # Render pagination links
     def render
       if auto_hide?
         nil
@@ -22,7 +20,6 @@ module SmartPagination
 
     private
 
-    # Default options
     def default_options
       {
         info_mode:      false,
@@ -43,7 +40,6 @@ module SmartPagination
       }
     end
 
-    # Get collection name
     def collection_name
       if @collection.is_a? ActiveRecord::Relation
         @collection.model_name.human(count: total_entries)
@@ -52,53 +48,43 @@ module SmartPagination
       end
     end
 
-    # Get current_page
     def current_page
       @collection.current_page.to_i
     end
 
-    # Get total pages
     def total_pages
       @total_pages ||= @collection.total_pages.to_i
     end
 
-    # Check if current page
     def current_page?(page)
       page.to_i == current_page
     end
 
-    # Get entries per page
     def per_page
       @collection.per_page.to_i
     end
 
-    # Get current entries
     def current_entries
       entries = current_page * per_page
       entries < total_entries ? entries : total_entries
     end
 
-    # Get total entries
     def total_entries
       @total_entries ||= @collection.total_entries.to_i
     end
 
-    # Check if should hide pagination
     def auto_hide?
       @options[:auto_hide].present? and total_pages < 2
     end
 
-    # Check if pager mode enabled
     def pager_mode?
       @options[:pager_mode].present?
     end
 
-    # Check if info mode enabled
     def info_mode?
       @options[:info_mode].present?
     end
 
-    # Current url parameters
     def url_params
       url    = URI.parse(@context.request.url)
       params = Rack::Utils.parse_nested_query(url.query).symbolize_keys
@@ -106,7 +92,6 @@ module SmartPagination
       params.except(:page)
     end
 
-    # Get page numbers
     def page_numbers
       inner = @options[:inner_window].to_i
       outer = @options[:outer_window].to_i
@@ -143,13 +128,11 @@ module SmartPagination
       left.to_a + middle.to_a + right.to_a
     end
 
-    # Get link params
     def link_params(page)
       page = @collection.send(:"#{page}_page") unless page.is_a? Integer
       { page: page }
     end
 
-    # Get link options
     def link_options(page)
       if page.is_a? Integer
         active = @options[:active_class] if current_page? page
@@ -162,22 +145,18 @@ module SmartPagination
       end
     end
 
-    # Page link
     def page_link(page)
       item_link page, link_params(page), link_options(page)
     end
 
-    # Previous link
     def previous_link
       item_link @options[:previous_text], link_params(:previous), link_options(:previous)
     end
 
-    # Next link
     def next_link
       item_link @options[:next_text], link_params(:next), link_options(:next)
     end
 
-    # Render item link
     def item_link(text, params = {}, html_options = {})
       wrapper  = @options[:item_wrapper]
       link_opt = html_options if wrapper.blank?
@@ -187,17 +166,14 @@ module SmartPagination
       item_tag
     end
 
-    # Render item separator
     def item_sep
       item_link '&hellip;', {}, class: "#{@options[:item_class]} #{@options[:disabled_class]}".strip
     end
 
-    # Render content tag
     def tag(*args, &block)
       @context.content_tag(*args, &block)
     end
 
-    # Render link tag
     def link_to(text, params = {}, html_options = {})
       prm = params.except(:page) if params[:page] == 1
       url = @context.url_for url_params.merge(prm || params)
@@ -207,7 +183,6 @@ module SmartPagination
       tag :a, "#{text}".html_safe, opt
     end
 
-    # Render wrapper
     def wrapper(links)
       wrapper  = @options[:wrapper]
       wrap_opt = { class: @options[:wrapper_class] }
@@ -217,7 +192,6 @@ module SmartPagination
       wrap_tag
     end
 
-    # Render pagination info
     def pagination_info
       current = tag :strong, current_entries, class: 'current'
       total   = tag :strong, total_entries, class: 'total'
@@ -225,13 +199,11 @@ module SmartPagination
       "Displaying #{current} of #{total} #{collection_name}".html_safe
     end
 
-    # Render pager
     def pager
       items = [previous_link, next_link]
       wrapper items.map(&:to_s).join
     end
 
-    # Render pagination
     def pagination
       items = page_numbers.map { |page| page == :sep ? item_sep : page_link(page) }
       items = [previous_link, *items, next_link]
